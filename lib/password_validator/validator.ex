d7d4652aa7579e0bcbd8ff2570a1700eb3f5662b
Jason Axelson <jason.axelson@gmail.com>
2019-04-05 08:09:06 -1000

    Minor stylistic improvement in validator function

--- lib/password_validator/validator.ex
+++ lib/password_validator/validator.ex
@@ -7,16 +7,15 @@ defmodule PasswordValidator.Validator do
   Validate the given string and return `:ok` or `{:error, errors}` where
   `errors` is a list.
   """
-  @callback validate(String.t(), list()) :: :ok | {:error, nonempty_list()}
+  @callback validate(String.t(), Keyword.t()) :: :ok | {:error, nonempty_list()}
 
   @spec return_errors_or_ok(list()) :: :ok | {:error, nonempty_list()}
   def return_errors_or_ok(results) do
     errors = for {:error, reason} <- results, do: reason
 
-    if length(errors) == 0 do
-      :ok
-    else
-      {:error, errors}
+    case errors do
+      [] -> :ok
+      _ -> {:error, errors}
     end
   end
 end
