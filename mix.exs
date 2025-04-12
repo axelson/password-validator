6b5448d1caf1440746cdde727c38601bd4b0f0c7
Jason Axelson <jason.axelson@gmail.com>
2025-04-12 09:04:56 -1000

    Bump version to 0.5.2

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule PasswordValidator.Mixfile do
   use Mix.Project
 
   @source_url "https://github.com/axelson/password-validator"
-  @version "0.5.1"
+  @version "0.5.2"
 
   def project do
     [
